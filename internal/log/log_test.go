d65a2f87541e2ac83ae4e5f6b595f8c5c006c43c
tamjidahmed <mail.to.tamjid@gmail.com>
2021-07-20 19:51:43 +0600

    Wrote more test for log

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -11,10 +11,11 @@ import (
 
 func TestLog(t *testing.T) {
 	for msg, fn := range map[string]func(t *testing.T, log *Log){
-		"append and read success":   testAppendRead,
-		"offset out of range error": testOutOfRangeErr,
-		"truncate":                  testTruncate,
-		"full reader":               testReader,
+		"append and read success":    testAppendRead,
+		"offset out of range error":  testOutOfRangeErr,
+		"truncate":                   testTruncate,
+		"full reader":                testReader,
+		"init with existing segment": testInitExistng,
 	} {
 		t.Run(msg, func(t *testing.T) {
 			dir, err := ioutil.TempDir("", "log-test")
@@ -79,4 +80,32 @@ func testReader(t *testing.T, log *Log) {
 	err = proto.Unmarshal(b[lenWidth:], read)
 	require.NoError(t, err)
 	require.Equal(t, rec.Value, read.Value)
+}
+
+func testInitExistng(t *testing.T, log *Log) {
+	rec := &api.Record{Value: []byte("hello world")}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(rec)
+		require.NoError(t, err)
+	}
+	require.NoError(t, log.Close())
+
+	lowOff1, err := log.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), lowOff1)
+
+	highOff1, err := log.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), highOff1)
+
+	log2, err := NewLog(log.Dir, log.Config)
+	require.NoError(t, err)
+
+	lowOff2, err := log2.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, lowOff1, lowOff2)
+
+	highOff2, err := log2.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, highOff1, highOff2)
 }
