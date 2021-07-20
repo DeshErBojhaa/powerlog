44eb8cb0653f8fbf708ea246707c738da5f88135
Tamjid Ahmed <mail.to.tamjid@gmail.com>
2021-07-20 13:32:49 +0600

    wrote skelaton for log store
    Signed-off-by: Tamjid Ahmed <mail.to.tamjid@gmail.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/DeshErBojhaa/powerlog
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0 // indirect
---
+++ store.go
@@ -0,0 +1,89 @@
+package powerlog
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var encoding = binary.BigEndian
+
+const lenWidth = 8 // Size of uint64
+
+type store struct {
+	*os.File
+	mu sync.Mutex
+	buf *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		buf:  bufio.NewWriter(f),
+		size: size,
+	}, nil
+}
+
+// Append adds entry to a buffered store before flushing
+// it to persistence storage i.e. file. This was number
+// of syscall is reduced.
+func (s *store) Append(p []byte) (uint64, uint64, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos := s.size
+	if err := binary.Write(s.buf, encoding, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, encoding.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// ReadAt reads len(p) bytes into p beginning at the off
+// offset in the store’s file..
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+// Close persists any buffered data before closing the file.
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+	return s.File.Close()
+}
