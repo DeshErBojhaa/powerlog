204c9416ff186adbc98510741277364894b911d4
tamjidahmed <mail.to.tamjid@gmail.com>
2021-07-22 19:04:36 +0600

    Implemented functionality for agent.

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,23 +1,125 @@
 package agent
 
 import (
+	"fmt"
+	api "github.com/DeshErBojhaa/powerlog/api/v1"
 	"github.com/DeshErBojhaa/powerlog/internal/discovery"
 	"github.com/DeshErBojhaa/powerlog/internal/log"
+	"github.com/DeshErBojhaa/powerlog/internal/server"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"net"
 	"sync"
 )
 
+// Agent runs on every service instance, setting
+// up and connecting all the different components
 type Agent struct {
 	Config
 
 	log        *log.Log
 	server     *grpc.Server
 	membership *discovery.Membership
-	replicator log.Replicator
+	replicator *log.Replicator
 
 	shutdown     bool
 	shutdownCh   chan struct{}
 	shutdownLock sync.Mutex
+}
+
+func (a *Agent) setupLogger() error {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return err
+	}
+	zap.ReplaceGlobals(logger)
+	return nil
+}
+
+func (a *Agent) setupLog() error {
+	var err error
+	a.log, err = log.NewLog(a.Config.DataDir, log.Config{})
+	return err
+}
+
+func (a *Agent) setupServer() error {
+	serverConfig := &server.Config{
+		CommitLog: a.log,
+	}
+	var opts []grpc.ServerOption
+	var err error
+	a.server, err = server.NewgRPCServer(serverConfig, opts...)
+	if err != nil {
+		return err
+	}
+	rpcAddr, err := a.RPCAddr()
+	if err != nil {
+		return err
+	}
+
+	ln, err := net.Listen("tcp", rpcAddr)
+	if err != nil {
+		return err
+	}
+	go func() {
+		if err := a.server.Serve(ln); err != nil {
+			_ = a.Shutdown()
+		}
+	}()
+	return err
+}
+
+func (a *Agent) Shutdown() error {
+	a.shutdownLock.Lock()
+	defer a.shutdownLock.Unlock()
+	if a.shutdown {
+		return nil
+	}
+	a.shutdown = true
+	close(a.shutdownCh)
+
+	serviceClosers := []func() error{
+		a.membership.Leave,
+		a.replicator.Close,
+		func() error {
+			a.server.GracefulStop()
+			return nil
+		},
+		a.log.Close,
+	}
+
+	for _, fn := range serviceClosers {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (a *Agent) setupMembership() error {
+	rpcAddr, err := a.Config.RPCAddr()
+	if err != nil {
+		return err
+	}
+	var opts []grpc.DialOption
+	conn, err := grpc.Dial(rpcAddr, opts...)
+	if err != nil {
+		return err
+	}
+	client := api.NewLogClient(conn)
+	a.replicator = &log.Replicator{
+		DialOptions: opts,
+		LocalServer: client,
+	}
+	a.membership, err = discovery.New(a.replicator, discovery.Config{
+		NodeName: a.Config.NodeName,
+		BindAddr: a.Config.BindAddr,
+		Tags: map[string]string{
+			"rpc_addr": rpcAddr,
+		},
+		StartJoinAddrs: a.Config.StartJoinAddrs,
+	})
+	return err
 }
 
 type Config struct {
@@ -26,4 +128,31 @@ type Config struct {
 	RPCPort        int
 	NodeName       string
 	StartJoinAddrs []string
+}
+
+func New(config Config) (*Agent, error) {
+	a := &Agent{
+		Config:     config,
+		shutdownCh: make(chan struct{}),
+	}
+	setup := []func() error{
+		a.setupLogger,
+		a.setupLog,
+		a.setupServer,
+		a.setupMembership,
+	}
+	for _, fn := range setup {
+		if err := fn(); err != nil {
+			return nil, err
+		}
+	}
+	return a, nil
+}
+
+func (c Config) RPCAddr() (string, error) {
+	host, _, err := net.SplitHostPort(c.BindAddr)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s:%d", host, c.RPCPort), nil
 }
