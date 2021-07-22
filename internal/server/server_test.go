d15ca88d59c3f2486ce95902a70c9cf403b39b28
tamjidahmed <mail.to.tamjid@gmail.com>
2021-07-22 12:54:28 +0600

    Wrote test for gRPC server

---
+++ internal/server/server_test.go
@@ -0,0 +1,79 @@
+package server
+
+import (
+	"context"
+	api "github.com/DeshErBojhaa/powerlog/api/v1"
+	"github.com/DeshErBojhaa/powerlog/internal/log"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"io/ioutil"
+	"net"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	for desc, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config){
+		"produce/consume message to/from log succeeds": func(t *testing.T, client api.LogClient, config *Config) {
+			ctx := context.Background()
+			want := &api.Record{Value: []byte("hello world")}
+			produce, err := client.Produce(
+				ctx,
+				&api.ProduceRequest{Record: want})
+			require.NoError(t, err)
+
+			consume, err := client.Consume(
+				ctx,
+				&api.ConsumeRequest{Offset: produce.Offset})
+			require.NoError(t, err)
+			require.Equal(t, want.Value, consume.Record.Value)
+			require.Equal(t, want.Offset, consume.Record.Offset)
+		},
+	} {
+		// Loop work
+		t.Run(desc, func(t *testing.T) {
+			client, config, teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
+
+func setupTest(t *testing.T, fn func(*Config)) (api.LogClient, *Config, func()) {
+	t.Helper()
+
+	l, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	dir, err := ioutil.TempDir("", "server-test")
+	require.NoError(t, err)
+
+	plog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	cfg := &Config{CommitLog: plog}
+	if fn != nil {
+		fn(cfg)
+	}
+
+	server, err := NewgRPCServer(cfg)
+	require.NoError(t, err)
+
+	go func() {
+		_ = server.Serve(l)
+	}()
+
+	clientOptions := []grpc.DialOption{grpc.WithInsecure()} // Fix later
+	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	require.NoError(t, err)
+	client := api.NewLogClient(cc)
+
+	return client, cfg, func() {
+		server.Stop()
+		_ = cc.Close()
+		_ = l.Close()
+		_ = plog.Remove()
+	}
+}
