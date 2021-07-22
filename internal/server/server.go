4a8e1409db14919d19d5aee2098a2b21e2abed74
tamjidahmed <mail.to.tamjid@gmail.com>
2021-07-22 11:33:44 +0600

    Implemented streams for gRPC server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,9 @@
 package server
 
-import api "github.com/DeshErBojhaa/powerlog/api/v1"
+import (
+	"context"
+	api "github.com/DeshErBojhaa/powerlog/api/v1"
+)
 
 //CommitLog describes the functionality for the gRPC server.
 type CommitLog interface {
@@ -24,4 +27,72 @@ func newgrpcServer(config *Config) (*grpcServer, error) {
 		Config: config,
 	}
 	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
+	*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+// ProduceStream implements a bidirectional streaming RPC
+// for the client to stream data into the server’s log and.
+// The server response with whether each request succeeded.
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+// ErrOffsetOutOfRange shut up IDE linter
+type ErrOffsetOutOfRange error
+
+// ConsumeStream implements a server-side streaming RPC so
+// the client can tell the server where in the log to read
+// records, and then the server will stream every record that
+// follows. When the server reaches the end of the log, the
+// server will wait until someone appends a record to the log
+// and then continue streaming records to the client.
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest,
+	stream api.Log_ConsumeStreamServer) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
 }
